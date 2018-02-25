463cc3bebe1592872080f8e937b9a447f49c9842
Jason Clark <riceboyler@gmail.com>
2018-02-25 15:05:22 -0500

    Fixed URL

--- front-end/src/dashboard/cards/SmartDollar.js
+++ front-end/src/dashboard/cards/SmartDollar.js
@@ -7,63 +7,70 @@ import SmartDollarVideo from "../../common/assets/SmartDollar.mp4";
 import SmartDollarModal from "./children/SmartDollarModal";
 
 class SmartDollar extends Component {
-  state = {
-    modalIsActive: false,
-    name: "",
-    email: "",
-    phone: "",
-    acceptTerms: false
-  };
+    state = {
+        modalIsActive: false,
+        name: "",
+        email: "",
+        phone: "",
+        acceptTerms: false
+    };
 
-  openModal = () => {
-    this.setState({ modalIsActive: true });
-  };
+    openModal = () => {
+        this.setState({ modalIsActive: true });
+    };
 
-  closeModal = () => {
-    this.setState({ modalIsActive: false });
-  };
+    closeModal = () => {
+        this.setState({ modalIsActive: false });
+    };
 
-  handleSubmit = formValues => {
-    this.setState({ formValues });
-  };
+    handleSubmit = formValues => {
+        this.setState({ formValues });
+    };
 
-  render() {
-    const { modalIsActive } = this.state;
-    const footerButtons = (
-      <CardFooter>
-        <CardFooterItem>
-          <Button isColor="primary" href="www.smartdollar.com/app/sign-in">
-            Login & LEARN
-          </Button>
-        </CardFooterItem>
-        <CardFooterItem>
-          <Button isColor="primary" isOutlined onClick={this.openModal}>
-            Request Access
-          </Button>
-        </CardFooterItem>
-      </CardFooter>
-    );
-    return (
-      <SingleColumnCard
-        title="Smart Dollar&copy;"
-        subtitleText="Powered by Ramsey Solutions"
-        footerContent={footerButtons}
-      >
-        <SmartDollarModal
-          modalIsActive={modalIsActive}
-          closeModal={this.closeModal}
-          onSubmit={this.handleSubmit}
-        />
-        <Player
-          ref="SmartDollarPlayer"
-          src="https://welfie.co/videos/smart-dollar-participant.WebM"
-          poster="http://i.stack.imgur.com/PtbGQ.png"
-        >
-          <BigPlayButton />
-        </Player>
-      </SingleColumnCard>
-    );
-  }
+    render() {
+        const { modalIsActive } = this.state;
+        const footerButtons = (
+            <CardFooter>
+                <CardFooterItem>
+                    <Button
+                        isColor="primary"
+                        href="http://www.smartdollar.com/app/sign-in"
+                    >
+                        Login & LEARN
+                    </Button>
+                </CardFooterItem>
+                <CardFooterItem>
+                    <Button
+                        isColor="primary"
+                        isOutlined
+                        onClick={this.openModal}
+                    >
+                        Request Access
+                    </Button>
+                </CardFooterItem>
+            </CardFooter>
+        );
+        return (
+            <SingleColumnCard
+                title="Smart Dollar&copy;"
+                subtitleText="Powered by Ramsey Solutions"
+                footerContent={footerButtons}
+            >
+                <SmartDollarModal
+                    modalIsActive={modalIsActive}
+                    closeModal={this.closeModal}
+                    onSubmit={this.handleSubmit}
+                />
+                <Player
+                    ref="SmartDollarPlayer"
+                    src="https://welfie.co/videos/smart-dollar-participant.WebM"
+                    poster="http://i.stack.imgur.com/PtbGQ.png"
+                >
+                    <BigPlayButton />
+                </Player>
+            </SingleColumnCard>
+        );
+    }
 }
 
 SmartDollar.propTypes = {};
